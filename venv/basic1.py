7cc75d23e06fdcfcb45062cbdad9b1fc42867e26
akasharkxx <akash.jaiswar41@gmail.com>
2019-08-01 23:20:26 +0530

    added new file showing placeholders

--- venv/basic1.py
+++ venv/basic1.py
@@ -5,7 +5,13 @@
 import tensorflow as tf
 
 const_node1 = tf.constant(1.0, dtype=tf.float32)
-const_node2 = tf.constant(2.0)
+const_node2 = tf.constant(2.0, dtype=tf.float32)
+const_node3 = tf.constant([3.0, 4.0, 5.0], dtype=tf.float32)
 
-print(const_node1)
-print(const_node2)
+adder_node_1 = tf.add(const_node1, const_node2)
+adder_node_2 = const_node1 + const_node2
+mult_node_1 = adder_node_1 * const_node3
+
+session = tf.Session()
+print(session.run(adder_node_1))
+print(session.run(mult_node_1 ))
---
+++ venv/basic2.py
@@ -0,0 +1,5 @@
+#Placeholder nodes
+#Nodes with no values
+#Pass in value when running session
+
+import tensorflow as tf
